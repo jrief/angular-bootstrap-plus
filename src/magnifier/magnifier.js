9e03d52f38534bf95e5d23fb5c0615bb11dbda57
Jacob Rief <jacob.rief@gmail.com>
2015-06-06 11:31:16 +0200

    better working prototype

--- src/magnifier/magnifier.js
+++ src/magnifier/magnifier.js
@@ -10,42 +10,47 @@ angular.module('bs-plus.magnifier', [])
 		},
 		transclude: true,
 		template: '<img src="{{ imageSrc }}" class="{{ cssClass }}" />' +
-		          '<div><img src="{{ imageSrc }}" /></div>',
+		          '<div class="{{ cssClass }}"><span></span></div>',
 		link: function(scope, element, attrs) {
 			var imageElem = element.find('img');
-			var imageRect = imageElem[0].getBoundingClientRect();
 			var canvasElem = element.find('div');
-			var plateElem = canvasElem.find('img');
+			var plateElem = canvasElem.find('span');
+			var magnify;
 			console.log(attrs['style']);
 			scope.cssClass = attrs['class'];
-			console.log(imageRect);
 			element.css({position: 'relative'});
 			canvasElem.css({
-				position: 'absolute',
-				top: 0, right: 0, bottom: 0, left: 0,
-				//'background-image': 'url("' + scope.imageSrc + '")',
-				//'background-repeat': 'no-repeat',
-				'opacity': 0,
-				overflow: 'hidden'
+				opacity: 0,
+				//overflow: 'hidden',
+				width: '100%',
+				height: '100%'
+				//transition: 'none'
 			});
 			plateElem.css({
 				position: 'absolute',
-				top: 0, left: 0,
-				width: imageElem.naturalWidth,
-				height: imageElem.naturalHeight
+				top: 0, right: 0, bottom: 0, left: 0,
+				'background-repeat': 'no-repeat'
 			});
-			canvasElem.on('mouseenter', function() {
+			element.on('mouseenter', function() {
 				console.log(element);
-				console.log(imageElem);
 				canvasElem.css('opacity', 1);
-			}).on('mousemove', function(evt) {
+				plateElem.css('background-image', 'url("' + scope.imageSrc + '")');
+				magnify = {
+					byX: (imageElem[0].naturalWidth - imageElem[0].clientWidth) / imageElem[0].clientWidth,
+					byY: (imageElem[0].naturalHeight - imageElem[0].clientHeight) / imageElem[0].clientHeight
+				};
+			});
+			canvasElem.on('mousemove', function(evt) {
+				//return;
 				//console.log('mousemove: ' + evt.offsetX + ', ' + evt.offsetY);
-				var posX = imageRect.left - evt.pageX; //imageElem[0].naturalWidth / imageElem[0].clientWidth;
-				var posY = imageRect.top - evt.pageY; //imageElem[0].naturalHeight / imageElem[0].clientHeight;
+				//var posX = imageRect.left - evt.pageX; // / imageElem[0].clientWidth;
+				//var posY = imageRect.top - evt.pageY; //imageElem[0].naturalHeight / imageElem[0].clientHeight;
+				var posX = -evt.offsetX * magnify.byX; //imageElem[0].naturalWidth / imageElem[0].clientWidth;
+				var posY = -evt.offsetY * magnify.byY; //imageElem[0].naturalHeight / imageElem[0].clientHeight;
 				console.log('mousemove: ' + posX + ', ' + posY);
-				//plateElem.css('background-position', posX + 'px ' + posY + 'px');
-				//plateElem.css('background-position', posX + 'px ' + posY + 'px');
-				plateElem.css({top: posY + 'px ', left: posX + 'px'});
+				plateElem.css('background-position', posX + 'px ' + posY + 'px');
+				//canvasElem.css('background-position', posX + 'px ' + posY + 'px');
+				//plateElem.css({top: posY + 'px ', left: posX + 'px'});
 			}).on('mouseout', function () {
 				console.log('mouseout');
 				canvasElem.css('opacity', 0);
