023c5fcf279dbac774a3ac9b744c3be2849e2c6e
Jacob Rief <jacob.rief@gmail.com>
2015-06-06 19:31:38 +0200

    first working magnifier

--- src/magnifier/magnifier.js
+++ src/magnifier/magnifier.js
@@ -8,15 +8,12 @@ angular.module('bs-plus.magnifier', [])
 		scope: {
 			imageSrc: '@'
 		},
-		transclude: true,
 		template: '<img src="{{ imageSrc }}" class="{{ cssClass }}" />' +
 		          '<div class="{{ cssClass }}"><span></span></div>',
 		link: function(scope, element, attrs) {
-			var imageElem = element.find('img');
 			var canvasElem = element.find('div');
 			var plateElem = canvasElem.find('span');
 			var magnify;
-			console.log(attrs['style']);
 			scope.cssClass = attrs['class'];
 			element.css({
 				position: 'relative',
@@ -28,44 +25,27 @@ angular.module('bs-plus.magnifier', [])
 				opacity: 0,
 				position: 'absolute',
 				top: 0, right: 0, bottom: 0, left: 0,
-				overflow: 'hidden',
-				//width: '100%',
-				//height: '100%'
-				//transition: 'none'
+				overflow: 'hidden'
+			});
+			plateElem.css({
+				width: '100%',
+				height: '100%',
+				display: 'inline-block'
 			});
 			canvasElem.on('mouseenter', function() {
-				console.log(element);
+				var imageElem = element.find('img');
 				canvasElem.css('opacity', 1);
 				plateElem.css('background-image', 'url("' + scope.imageSrc + '")');
-/*
-				plateElem.css({
-					width: imageElem[0].naturalWidth,
-					height: imageElem[0].naturalHeight
-				});
-*/
-				plateElem.css({
-					width: '100%',
-					height: '100%',
-					display: 'inline-block'
-				});
 				magnify = {
 					byX: (imageElem[0].naturalWidth - imageElem[0].clientWidth) / imageElem[0].clientWidth,
 					byY: (imageElem[0].naturalHeight - imageElem[0].clientHeight) / imageElem[0].clientHeight
 				};
 			});
 			plateElem.on('mousemove', function(evt) {
-				//console.log('mousemove: ' + evt.offsetX + ', ' + evt.offsetY);
-				//var posX = imageRect.left - evt.pageX; // / imageElem[0].clientWidth;
-				//var posY = imageRect.top - evt.pageY; //imageElem[0].naturalHeight / imageElem[0].clientHeight;
-				var posX = -evt.offsetX * magnify.byX; //imageElem[0].naturalWidth / imageElem[0].clientWidth;
-				var posY = -evt.offsetY * magnify.byY; //imageElem[0].naturalHeight / imageElem[0].clientHeight;
-				console.log('mousemove: ' + posX + ', ' + posY);
+				var posX = -evt.offsetX * magnify.byX;
+				var posY = -evt.offsetY * magnify.byY;
 				plateElem.css('background-position', posX + 'px ' + posY + 'px');
-				//canvasElem.css('background-position', posX + 'px ' + posY + 'px');
-				//canvasElem.css({'top': posY + 'px ', 'left': posX + 'px'});
-				//plateElem.css({'margin-top': posY + 'px ', 'margin-left': posX + 'px'});
 			}).on('mouseout', function () {
-				console.log('mouseout');
 				canvasElem.css('opacity', 0);
 			});
 		}
