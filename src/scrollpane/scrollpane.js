b3abaa55d4214b64acf0a81cb08e2b4a5eec16d6
Jacob Rief <jacob.rief@gmail.com>
2016-08-15 15:28:34 +0200

    better margin handling

--- src/scrollpane/scrollpane.js
+++ src/scrollpane/scrollpane.js
@@ -4,34 +4,35 @@
 var module = angular.module('bs-plus.scrollpane', []);
 
 module.directive('bspScrollPane', ['$timeout', '$window', function($timeout, $window) {
-	var child, delta, maxOffset, offset, initial;
-
-	function setDefaults() {
-		maxOffset = child.scrollWidth - child.offsetWidth;
-		offset = maxOffset * initial;
-		delta = child.offsetWidth / 3;
-		$timeout(setOffset, 250);
-	}
-
-	function setOffset() {
-		offset = Number.isNaN(offset) ? 0 : Math.min(Math.max(offset, 0), maxOffset);
-		angular.element(child).css('margin-left', -offset + 'px');
-	}
-
 	return {
 		transclude: true,
 		templateUrl: 'scrollpane/bsp-scrollpane.tmpl.html',
 		link: function(scope, element, attrs) {
+			var child, delta, maxOffset, offset, initial, margin;
+
+			function setDefaults() {
+				maxOffset = child.scrollWidth - child.offsetWidth;
+				offset = maxOffset * initial;
+				delta = child.offsetWidth / 3;
+				$timeout(setOffset, 250);
+			}
+
+			function setOffset() {
+				offset = Number.isNaN(offset) ? -margin : Math.min(Math.max(offset, -margin), maxOffset);
+				angular.element(child).css('margin-left', -offset + 'px');
+			}
+
 			child = element.children('ng-transclude').children();
 			if (child.length !== 3)
 				throw new Error("A 'bsp-scroll-pane' directive may contain only one element");
 			child = child[1];
 			initial = attrs.initialPercentage / 100;
+			margin = child.offsetLeft;
 
 			angular.element($window).bind('resize', setDefaults);
 			setDefaults();
 
-			element.find('div').on('click', function(event) {
+			element.find('scroll-edge').on('click', function(event) {
 				switch (event.target.getAttribute('class')) {
 					default:
 						return;
@@ -42,7 +43,7 @@ module.directive('bspScrollPane', ['$timeout', '$window', function($timeout, $wi
 						offset += delta;
 						break;
 				}
-				offset = Math.min(Math.max(offset, -25), maxOffset + 25);
+				offset = Math.min(Math.max(offset, -margin-25), maxOffset+25);
 				angular.element(child).css('margin-left', -offset + 'px');
 				$timeout(setOffset, 250);
 			});
